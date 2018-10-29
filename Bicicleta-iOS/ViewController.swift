ada063784037adf731641c51986aa957f9747938
Lluis Casals <baderetro@gmail.com>
2018-10-29 23:19:04 +0100

    Initial commit

---
+++ Bicicleta-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bicicleta-iOS
+//
+//  Created by Lluís Xavier Casals García on 29/10/2018.
+//  Copyright © 2018 Lluís Xavier Casals García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
